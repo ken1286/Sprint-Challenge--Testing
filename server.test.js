8d57bfbec7c2a05d5513035cb77507f34de37eb6
ken1286 <kindle1286@gmail.com>
2019-06-21 08:46:35 -0700

    added it responds with a list of games test

--- server.test.js
+++ server.test.js
@@ -4,11 +4,35 @@ const supertest = require('supertest');
 describe('server', () => {
   describe('GET /games', () => {
     it('responds with 200 OK', () => {
-
       return supertest(server)
         .get('/games')
         .expect(200);
+    })
 
+    it('responds with a list of games', () => {
+      let gamesList = [
+        {
+          title: 'Pacman', // required
+          genre: 'Arcade', // required
+          releaseYear: 1980 // not required
+        },
+        {
+          title: 'Chrono Trigger', // required
+          genre: 'RPG', // required
+          releaseYear: 1995 // not required
+        },
+        {
+          title: 'Donkey Kong Country', // required
+          genre: 'Platformer', // required
+          releaseYear: 1994 // not required
+        },
+      ];
+
+      return supertest(server)
+        .get('/games')
+        .then(res => {
+          expect(res.body).toBe(gamesList);
+        });
     })
 
     it('responds with json format', () => {
